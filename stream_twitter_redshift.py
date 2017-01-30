09ab89a9583dade73c2f7b44aee25586121d7aec
Harold L <hleond20@gmail.com>
2017-01-30 00:46:14 -0500

    DB connection
    This changes include the processing data function and store data into db.

--- stream_twitter_redshift.py
+++ stream_twitter_redshift.py
@@ -21,7 +21,7 @@ import threading
 
 
 lista_data = []
-
+lista_data_alt = []
 
 ckey = 'gHc857rgGXVeXIOaPrw6JrHrt'
 csecret = 'Ehug0Ogf3Y5c2CrDlHt3sZOPmu55AMLY3TFxzcUhrfISwyg0vx'
@@ -53,6 +53,9 @@ class listener(StreamListener):
     
     def on_data(self, data):
         try:
+            with open('tweets.txt', 'a') as f:
+                f.write(data)
+            lista_data_alt.append(data)
             ##print(data)
             ##return True
             
@@ -62,135 +65,12 @@ class listener(StreamListener):
 
             ##print(json.dumps(dataJson, indent=4)) 
             ##return Truejson_load = json.loads(data)
-            json_load = json.loads(data)
             
-            if 'text' in json_load:
-                texts = json_load['text']
-            else:
-                texts = ''
-            #coded = texts.encode('utf-8')
-            #s = str(coded)
-            tweet = str(texts.encode('utf-8'))
-            
-            if 'created_at' in json_load:
-                if json_load['created_at'] is None:
-                    tweetDate = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %z %Y")
-                else:
-                    tweetDate = str(json_load['created_at'])
-            else:
-                tweetDate = datetime.now().strftime("%a %b %d %H:%M:%S %z %Y")
-                
-            parsed_date = parser.parse(tweetDate)
-            # Obtenemos la fecha y la hora del tuit por separado
-            fechaTuit = parsed_date.strftime("%Y-%m-%d")
-            horaTuit = parsed_date.strftime("%H:%M:%S")
-
-            ## ID DEL TUIT
-            if 'id' in json_load:
-                tweetId = str(json_load['id'])
-            else:
-                tweetId = ""
-            #print(tweetId)
-            
-            ## USUARIO DEL TUIT
-            if 'user' not in json_load:
-                userName = ""
-                followers = 0
-                
-            else:
-                ## NOMBRE DEL USUARIO
-                if 'screen_name' in json_load['user']:
-                    userName = str(json_load['user']['screen_name'])
-                else:
-                    userName = ""
-                    
-                ## FOLLOWERS DEL USUARIO
-                if 'followers_count' in json_load['user']:
-                    followers = int(json_load['user']['followers_count'])
-                else:
-                    followers = 0
-            
-           
-            # FAVORITOS DEL TUIT
-            if 'favorite_count' not in json_load:
-                favoritos = 0
-            else:
-                favoritos = int(json_load['favorite_count'])
-
-            
-            ## RETUITS
-            if 'retweet_count' not in json_load:
-                retweets = 0
-            else:
-                retweets = int(json_load['retweet_count'])
-
-            ## UBICACIÓN DEL TUIT         
-            latitud = 0.0000
-            longitud = 0.0000
-
-            if 'geo' not in json_load:
-                latitud = 0.0000
-                longitud = 0.0000
-            else:
-                if json_load['geo'] is None:
-                    latitud = 0.0000
-                    longitud = 0.0000                    
-                else:
-                    latitud = float(json_load['geo']['coordinates'][1])
-                    longitud = float(json_load['geo']['coordinates'][0])
-                    
-
-            ## PAÍS
-            if 'place' in json_load:
-                location = json_load['place']
-            else:
-                location = None
-
-            if location is None:
-                pais = ''
-                ubicacion = ''
-            else:
-                pais = str(json_load['place']['country'])
-                ubicacion = str(json_load['place']['full_name'])
-        
-                
-            candidato = 0
-
-            
-            
-            ## insertamos la data
-            ##cursor.execute("""INSERT INTO bigdatatwitter(codigo,fecha,hora,usuario,followers,mensaje,favoritos,retweets,country,ubicacion,latitud,longitud, candidato)
-##VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s);""",[tweetId, fechaTuit, horaTuit, userName, followers, tweet, favoritos, retweets, pais,
-##                                                            ubicacion, latitud,longitud, candidato])
-##            conn.commit()
-            
-            data_collection = OrderedDict()
-            data_collection['id'] = tweetId
-            data_collection['fecha'] = fechaTuit
-            data_collection['hora'] = horaTuit
-            data_collection['usuario'] = userName
-            data_collection['followers'] = followers
-            data_collection['mensaje'] = tweet
-            data_collection['favoritos'] = favoritos
-            data_collection['retweets'] = retweets
-            data_collection['country'] = pais
-            data_collection['ubicacion'] =  ubicacion
-            data_collection['latitud'] = latitud
-            data_collection['longitud'] = longitud
-            data_collection['candidato'] = candidato
-
-            lista_data.append(data_collection)
-
-            print(len(lista_data))
-            return True
-        
-            #self.trending_words()
-            #time.sleep(5)
-            #cursor.close()
             return True
         except BaseException as e:
             print('failed ondata,' + str(e))
-            
+            ## Procesamos toda la data restante en el array
+            insertar_data(True)                    
             time.sleep(5)
     
     def on_error(self, status):
@@ -238,18 +118,49 @@ def trending_words():
             print('error,' + str(e))
 
 
-def insertar_data():
+def insertar_data(all=False):
     try:
         #cursor = conn.cursor()
-        if len(lista_data) > 0:
-            
-            for item in lista_data:
-                print(item)
+        i = 0
+        n = 1000
+        lista = lista_data_alt
+        longitud = len(lista)
+        
+        if longitud > 0:
+            print("hay elementos")
+            if all == True:
+                for item in lista:
+                    with open('data_alt.txt', 'a') as f:
+                        #print(item['usuario'])
+                        data_procesada = procesar_data_tuits(item)
+                        f.write(data_procesada['usuario'] + "\n")
+                        insertar_en_bd(data_procesada)
                 
-             ## insertamos la data
-           ## cursor.execute("""INSERT INTO bigdatatwitter(codigo,fecha,hora,usuario,followers,mensaje,favoritos,retweets,country,ubicacion,latitud,longitud, candidato)
-    ##VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s);""",[tweetId, fechaTuit, horaTuit, userName, followers, tweet, favoritos, retweets, pais,
-      ##                                                          ubicacion, latitud,longitud, candidato])
+            else:               
+                for item in lista:
+                    if i <= n:
+                        with open('data_alt.txt', 'a') as f:
+                            #print(item['usuario'])
+                            data_procesada = procesar_data_tuits(item)
+                            f.write(data_procesada['usuario'] + "\n") 
+                            insertar_en_bd(data_procesada)
+                                                 
+                    else:
+                        break                      
+                    
+                i = i + 1
+        else:
+            print("no hay elementos")
+
+
+
+        if longitud >= n:
+            del lista_data_alt[:n]
+        else:
+            del lista_data_alt[:longitud]
+
+            
+        
             
         #conn.commit()
         #cursor.close()
@@ -257,12 +168,172 @@ def insertar_data():
         #lista_data = []
         #print("palabras comunes")
         #print(len(lista_data))
-        threading.Timer(5.0, insertar_data).start()
+        threading.Timer(7.0, insertar_data).start()
     except BaseException as e:
             print('error,' + str(e))
+
+def insertar_en_bd(data_collection):
+
+    try:
         
+        tweetId = data_collection['id']
+        fechaTuit = data_collection['fecha']
+        horaTuit = data_collection['hora']
+        userName = data_collection['usuario']
+        followers = data_collection['followers']
+        tweet = data_collection['mensaje']
+        favoritos = data_collection['favoritos']
+        retweets = data_collection['retweets']
+        pais = data_collection['country']
+        ubicacion = data_collection['ubicacion']
+        latitud = data_collection['latitud']
+        longitud = data_collection['longitud']
+        candidato = data_collection['candidato']
+
+        cursor = conn.cursor()
+        ## insertamos la data
+        cursor.execute("""INSERT INTO bigdatatwitter(codigo,fecha,hora,usuario,followers,mensaje,favoritos,retweets,country,ubicacion,latitud,longitud, candidato)
+VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s);""",[tweetId, fechaTuit, horaTuit, userName, followers, tweet, favoritos, retweets, pais,
+                                                            ubicacion, latitud,longitud, candidato])
+        conn.commit()
+        cursor.close()
+
+    except BaseException as e:
+        print('error al insertar,' + str(e))
+
+
+def procesar_data_tuits(data):
+    
+    json_load = json.loads(data)
+            
+    if 'text' in json_load:
+        texts = json_load['text']
+    else:
+        texts = ''
+    #coded = texts.encode('utf-8')
+    #s = str(coded)
+    tweet = str(texts.encode('utf-8'))
+    
+    if 'created_at' in json_load:
+        if json_load['created_at'] is None:
+            tweetDate = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %z %Y")
+        else:
+            tweetDate = str(json_load['created_at'])
+    else:
+        tweetDate = datetime.now().strftime("%a %b %d %H:%M:%S %z %Y")
+        
+    parsed_date = parser.parse(tweetDate)
+    # Obtenemos la fecha y la hora del tuit por separado
+    fechaTuit = parsed_date.strftime("%Y-%m-%d")
+    horaTuit = parsed_date.strftime("%H:%M:%S")
+
+    ## ID DEL TUIT
+    if 'id' in json_load:
+        tweetId = str(json_load['id'])
+    else:
+        tweetId = ""
+    #print(tweetId)
+    
+    ## USUARIO DEL TUIT
+    if 'user' not in json_load:
+        userName = ""
+        followers = 0
+        
+    else:
+        ## NOMBRE DEL USUARIO
+        if 'screen_name' in json_load['user']:
+            userName = str(json_load['user']['screen_name'])
+        else:
+            userName = ""
+            
+        ## FOLLOWERS DEL USUARIO
+        if 'followers_count' in json_load['user']:
+            followers = int(json_load['user']['followers_count'])
+        else:
+            followers = 0
+    
+   
+    # FAVORITOS DEL TUIT
+    if 'favorite_count' not in json_load:
+        favoritos = 0
+    else:
+        favoritos = int(json_load['favorite_count'])
+
+    
+    ## RETUITS
+    if 'retweet_count' not in json_load:
+        retweets = 0
+    else:
+        retweets = int(json_load['retweet_count'])
+
+    ## UBICACIÓN DEL TUIT         
+    latitud = 0.0000
+    longitud = 0.0000
+
+    if 'geo' not in json_load:
+        latitud = 0.0000
+        longitud = 0.0000
+    else:
+        if json_load['geo'] is None:
+            latitud = 0.0000
+            longitud = 0.0000                    
+        else:
+            latitud = float(json_load['geo']['coordinates'][1])
+            longitud = float(json_load['geo']['coordinates'][0])
+            
+
+    ## PAÍS
+    if 'place' in json_load:
+        location = json_load['place']
+    else:
+        location = None
+
+    if location is None:
+        pais = ''
+        ubicacion = ''
+    else:
+        pais = str(json_load['place']['country'])
+        ubicacion = str(json_load['place']['full_name'])
+
+        
+    candidato = 0
+
+    
+    
+    ## insertamos la data
+    ##cursor.execute("""INSERT INTO bigdatatwitter(codigo,fecha,hora,usuario,followers,mensaje,favoritos,retweets,country,ubicacion,latitud,longitud, candidato)
+##VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s);""",[tweetId, fechaTuit, horaTuit, userName, followers, tweet, favoritos, retweets, pais,
+##                                                            ubicacion, latitud,longitud, candidato])
+##            conn.commit()
+    
+    data_collection = OrderedDict()
+    data_collection['id'] = tweetId
+    data_collection['fecha'] = fechaTuit
+    data_collection['hora'] = horaTuit
+    data_collection['usuario'] = userName
+    data_collection['followers'] = followers
+    data_collection['mensaje'] = tweet
+    data_collection['favoritos'] = favoritos
+    data_collection['retweets'] = retweets
+    data_collection['country'] = pais
+    data_collection['ubicacion'] =  ubicacion
+    data_collection['latitud'] = latitud
+    data_collection['longitud'] = longitud
+    data_collection['candidato'] = candidato
+
+    return data_collection
+
+    #print(len(lista_data))
+
+    #self.trending_words()
+    #time.sleep(5)
+    #cursor.close()
+
+
+    
 #trending_words()
 insertar_data()
+
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
